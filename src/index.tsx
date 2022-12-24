1735060228d4176683cafdbaefe7c65d652e981b
TheLimePixel <TheLimePixel@users.noreply.github.com>
2022-12-24 22:26:46 +0000

    deploy: 357c62715fec8fb9ed97c4e8a02a5c56b44da0ff

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './sass/index.scss'
-import { App } from './app'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App/>)
